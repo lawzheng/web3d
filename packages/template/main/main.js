7fe5986a014035a73c63f67919e860ace8b72708
lawzheng <602285510@qq.com>
2022-11-26 17:25:19 +0800

    add template

---
+++ packages/template/main/main.js
@@ -0,0 +1,50 @@
+import * as THREE from 'three'
+// 轨道控制器
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GUI } from 'dat.gui'
+const gui = new GUI();
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+camera.position.set(0, 0, 10)
+scene.add(camera)
+
+
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+
+const controls = new OrbitControls(camera, renderer.domElement)
+// 开启阻尼 更真实
+controls.enableDamping = true;
+
+// 坐标轴
+const axesHelper = new THREE.AxesHelper( 5 );
+scene.add( axesHelper );
+
+controls.update();
+
+function animate() {
+  controls.update();
+	renderer.render( scene, camera );
+	requestAnimationFrame( animate );
+
+}
+animate()
+
+window.addEventListener('resize', () => {
+  // 更新宽高比
+  camera.aspect = window.innerWidth / window.innerHeight
+  // 更新摄像机投影矩阵
+  camera.updateProjectionMatrix();
+  // 更新渲染器
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  // 更新像素比
+  renderer.setPixelRatio = window.devicePixelRatio;
+})
