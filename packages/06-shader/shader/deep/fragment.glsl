4beb6eeb7a8aaff5815781a2cf2239319c57df7d
lawzheng <602285510@qq.com>
2022-12-11 20:59:03 +0800

    add noise shader

--- packages/06-shader/shader/deep/fragment.glsl
+++ packages/06-shader/shader/deep/fragment.glsl
@@ -17,6 +17,28 @@ vec2 rotate(vec2 uv, float rotation, vec2 mid)
   );
 }
 
+float noise (in vec2 st) {
+  vec2 i = floor(st);
+  vec2 f = fract(st);
+
+  // Four corners in 2D of a tile
+  float a = random(i);
+  float b = random(i + vec2(1.0, 0.0));
+  float c = random(i + vec2(0.0, 1.0));
+  float d = random(i + vec2(1.0, 1.0));
+
+  // Smooth Interpolation
+
+  // Cubic Hermine Curve.  Same as SmoothStep()
+  vec2 u = f*f*(3.0-2.0*f);
+  // u = smoothstep(0.,1.,f);
+
+  // Mix 4 coorners percentages
+  return mix(a, b, u.x) +
+          (c - a)* u.y * (1.0 - u.x) +
+          (d - b) * u.x * u.y;
+}
+
 uniform float uTime;
 
 void main() {
@@ -166,7 +188,19 @@ void main() {
   // gl_FragColor = vec4(strength, strength, strength, 1);
 
   // 光芒四射
-  float angle = atan(vUv.x - 0.5, vUv.y - 0.5) / (2.0 * PI);
-  float strength = sin(angle * 100.0);
-  gl_FragColor = vec4(strength, strength, strength, 1);
+  // float angle = atan(vUv.x - 0.5, vUv.y - 0.5) / (2.0 * PI);
+  // float strength = sin(angle * 100.0);
+  // gl_FragColor = vec4(strength, strength, strength, 1);
+
+  // 噪声
+  // float strength = step(0.5, noise(vUv * 10.0 + uTime));
+  // gl_FragColor = vec4(strength, strength, strength, 1);
+
+  // 颜色混合
+  vec3 blackColor = vec3(1.0,0.0,1.0);
+  vec3 yellowColor = vec3(0.0,1.0,0.0);
+  vec3 uvColor = vec3(vUv,1.0);
+  float strength = noise(vUv * 100.0);
+  vec3 mixColor = mix(uvColor, yellowColor, strength);
+  gl_FragColor = vec4(mixColor, 1);
 }
